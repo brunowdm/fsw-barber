1709417e765778547e3e85180880cc0881e3316b
brunowdm <brunowam@gmail.com>
2024-08-08 15:36:28 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
+import { Button } from "./_components/ui/button";
 
 export default function Home() {
   return (
-    <h1 className="text-red-500 mb-5 bg-red-400">
-      Teste
-    </h1>
+    <Button>Teste</Button> 
   );
 }

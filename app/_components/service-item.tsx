14335aacbbeedf3de3c442bd7ebe72d6eb8e258b
brunowdm <brunowam@gmail.com>
2024-08-26 15:37:00 -0300

    feat: ensure only signed in user can booking

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -13,6 +13,8 @@ import { createBooking } from "../_actions/create-booking";
 import { useSession } from "next-auth/react";
 import { toast } from "sonner";
 import { getBookings } from "../_actions/get-bookings";
+import { Dialog, DialogContent } from "./ui/dialog";
+import SignInDialog from "./sign-in-dialog";
 
 interface ServiceItemProps {
     service: BarbershopService
@@ -51,6 +53,7 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
     const [selectedTime, setselectedTime] = useState<string | undefined>(undefined);
     const [dayBookings, setDayBookings] = useState<Booking[]>([]);
     const [bookingSheeetIsOpen, setBookingSheeetIsOpen] = useState(false)
+    const [signInDialogIsOpen, setSignInDialogIsOpen] = useState(false)
 
     useEffect(() => {
         const fetch = async () => {
@@ -61,6 +64,14 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
 
         fetch()
     }, [selectedDay, service.id])
+
+    const handleBookingClick = () =>{
+        if(data?.user){
+            return setBookingSheeetIsOpen(true)
+        }
+
+        return setSignInDialogIsOpen(true)
+    }
 
 
 
@@ -127,124 +138,132 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
     }
 
     return (
-        <Card>
-            <CardContent className="flex items-center gap-3 p-3">
+        <>
+            <Card>
+                <CardContent className="flex items-center gap-3 p-3">
 
-                <div className="relative min-h-[110px] min-w-[110px] max-h-[110px] max-w-[110px]">
-                    <Image alt={service.name} src={service.imageUrl} fill className="object-cover rounded-lg" />
-                </div>
-
-                <div className="space-y-2">
-                    <h3 className="font-semibold text-sm">{service.name}</h3>
-                    <p className="text-sm text-gray-400">{service.description}</p>
-
-                    <div className="flex items-center justify-between">
-                        <p className="text-sm font-bold text-primary">{Intl.NumberFormat("pt-BR", {
-                            style: "currency",
-                            currency: "BRL"
-                        }).format(Number(service.price))} </p>
-
-                        <Sheet open={bookingSheeetIsOpen} onOpenChange={handleBookingSheeetIsOpen}>
-
-                            <Button variant="secondary" size="sm" onClick={() => setBookingSheeetIsOpen(true)}>Reservar</Button>
-
-                            <SheetContent className="px-0">
-                                <SheetHeader>
-                                    <SheetTitle>Fazer Reserva</SheetTitle>
-                                </SheetHeader>
-
-                                <div className="py-5 border-b border-solid">
-                                    <Calendar
-                                        mode="single"
-                                        locale={ptBR}
-                                        selected={selectedDay}
-                                        onSelect={handleDateSelect}
-                                        fromDate={addDays(new Date(), 1)}
-                                        styles={{
-                                            head_cell: {
-                                                width: "100%",
-                                                textTransform: "capitalize",
-                                            },
-                                            cell: {
-                                                width: "100%",
-                                            },
-                                            button: {
-                                                width: "100%",
-                                            },
-                                            nav_button_previous: {
-                                                width: "32px",
-                                                height: "32px",
-                                            },
-                                            nav_button_next: {
-                                                width: "32px",
-                                                height: "32px",
-                                            },
-                                            caption: {
-                                                textTransform: "capitalize",
-                                            },
-                                        }}
-                                    />
-                                </div>
-
-                                {selectedDay && (
-                                    <div className="p-5 flex overflow-x-auto gap-3 border-b border-solid">
-                                        {getTimeList({ bookings: dayBookings, selectedDay }).map((time) => (
-                                            <Button key={time} variant={selectedTime == time ? "default" : "outline"} className="rounded-full" onClick={() => handleTimeSelected(time)}>{time}</Button>
-                                        ))}
-                                    </div>
-                                )}
-
-                                {selectedTime && (
-                                    <div className="p-5">
-                                        <Card>
-                                            <CardContent className="p-3 space-y-3">
-                                                <div className="flex justify-between items-center">
-                                                    <h2 className="font-bold">{service.name}</h2>
-                                                    <p className="text-sm font-bold">
-                                                        {Intl.NumberFormat("pt-BR", {
-                                                            style: "currency",
-                                                            currency: "BRL"
-                                                        }).format(Number(service.price))}
-                                                    </p>
-                                                </div>
-
-                                                <div className="flex justify-between items-center">
-                                                    <h2 className="text-sm text-gray-400">Data</h2>
-                                                    <p className="text-sm">
-                                                        {format(selectedDay, "d 'de' MMMM", {
-                                                            locale: ptBR
-                                                        })}
-                                                    </p>
-                                                </div>
-
-                                                <div className="flex justify-between items-center">
-                                                    <h2 className="text-sm text-gray-400">Horário</h2>
-                                                    <p className="text-sm">
-                                                        {selectedTime}
-                                                    </p>
-                                                </div>
-
-                                                <div className="flex justify-between items-center">
-                                                    <h2 className="text-sm text-gray-400">Barbearia</h2>
-                                                    <p className="text-sm">
-                                                        {barbershop.name}
-                                                    </p>
-                                                </div>
-                                            </CardContent>
-                                        </Card>
-                                    </div>
-                                )}
-
-                                <SheetFooter className="px-5 mt-5">
-                                    <Button onClick={handleCreateBooking} disabled={!selectedDay || !selectedTime}>Confirmar</Button>
-                                </SheetFooter>
-
-                            </SheetContent>
-                        </Sheet>
+                    <div className="relative min-h-[110px] min-w-[110px] max-h-[110px] max-w-[110px]">
+                        <Image alt={service.name} src={service.imageUrl} fill className="object-cover rounded-lg" />
                     </div>
-                </div>
-            </CardContent>
-        </Card>
+
+                    <div className="space-y-2">
+                        <h3 className="font-semibold text-sm">{service.name}</h3>
+                        <p className="text-sm text-gray-400">{service.description}</p>
+
+                        <div className="flex items-center justify-between">
+                            <p className="text-sm font-bold text-primary">{Intl.NumberFormat("pt-BR", {
+                                style: "currency",
+                                currency: "BRL"
+                            }).format(Number(service.price))} </p>
+
+                            <Sheet open={bookingSheeetIsOpen} onOpenChange={handleBookingSheeetIsOpen}>
+
+                                <Button variant="secondary" size="sm" onClick={handleBookingClick}>Reservar</Button>
+
+                                <SheetContent className="px-0">
+                                    <SheetHeader>
+                                        <SheetTitle>Fazer Reserva</SheetTitle>
+                                    </SheetHeader>
+
+                                    <div className="py-5 border-b border-solid">
+                                        <Calendar
+                                            mode="single"
+                                            locale={ptBR}
+                                            selected={selectedDay}
+                                            onSelect={handleDateSelect}
+                                            fromDate={new Date()}
+                                            styles={{
+                                                head_cell: {
+                                                    width: "100%",
+                                                    textTransform: "capitalize",
+                                                },
+                                                cell: {
+                                                    width: "100%",
+                                                },
+                                                button: {
+                                                    width: "100%",
+                                                },
+                                                nav_button_previous: {
+                                                    width: "32px",
+                                                    height: "32px",
+                                                },
+                                                nav_button_next: {
+                                                    width: "32px",
+                                                    height: "32px",
+                                                },
+                                                caption: {
+                                                    textTransform: "capitalize",
+                                                },
+                                            }}
+                                        />
+                                    </div>
+
+                                    {selectedDay && (
+                                        <div className="p-5 flex overflow-x-auto gap-3 border-b border-solid">
+                                            {getTimeList({ bookings: dayBookings, selectedDay }).map((time) => (
+                                                <Button key={time} variant={selectedTime == time ? "default" : "outline"} className="rounded-full" onClick={() => handleTimeSelected(time)}>{time}</Button>
+                                            ))}
+                                        </div>
+                                    )}
+
+                                    {selectedTime && (
+                                        <div className="p-5">
+                                            <Card>
+                                                <CardContent className="p-3 space-y-3">
+                                                    <div className="flex justify-between items-center">
+                                                        <h2 className="font-bold">{service.name}</h2>
+                                                        <p className="text-sm font-bold">
+                                                            {Intl.NumberFormat("pt-BR", {
+                                                                style: "currency",
+                                                                currency: "BRL"
+                                                            }).format(Number(service.price))}
+                                                        </p>
+                                                    </div>
+
+                                                    <div className="flex justify-between items-center">
+                                                        <h2 className="text-sm text-gray-400">Data</h2>
+                                                        <p className="text-sm">
+                                                            {format(selectedDay, "d 'de' MMMM", {
+                                                                locale: ptBR
+                                                            })}
+                                                        </p>
+                                                    </div>
+
+                                                    <div className="flex justify-between items-center">
+                                                        <h2 className="text-sm text-gray-400">Horário</h2>
+                                                        <p className="text-sm">
+                                                            {selectedTime}
+                                                        </p>
+                                                    </div>
+
+                                                    <div className="flex justify-between items-center">
+                                                        <h2 className="text-sm text-gray-400">Barbearia</h2>
+                                                        <p className="text-sm">
+                                                            {barbershop.name}
+                                                        </p>
+                                                    </div>
+                                                </CardContent>
+                                            </Card>
+                                        </div>
+                                    )}
+
+                                    <SheetFooter className="px-5 mt-5">
+                                        <Button onClick={handleCreateBooking} disabled={!selectedDay || !selectedTime}>Confirmar</Button>
+                                    </SheetFooter>
+
+                                </SheetContent>
+                            </Sheet>
+                        </div>
+                    </div>
+                </CardContent>
+            </Card>
+
+            <Dialog open={signInDialogIsOpen} onOpenChange={(open) => setSignInDialogIsOpen(open)}>
+                <DialogContent className="w-[90%]">
+                    <SignInDialog />
+                </DialogContent>
+            </Dialog>
+        </>
     );
 }
 
--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -10,6 +10,7 @@ import Image from "next/image";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "./ui/dialog";
 import { DialogTrigger } from "@radix-ui/react-dialog";
 import { signIn, signOut, useSession } from "next-auth/react";
+import SignInDialog from "./sign-in-dialog";
 
 const SideBar = () => {
 
@@ -47,17 +48,7 @@ const SideBar = () => {
                                 </Button>
                             </DialogTrigger>
                             <DialogContent className="w-[90%]">
-                                <DialogHeader>
-                                    <DialogTitle>Faça login na plataforma</DialogTitle>
-                                    <DialogDescription>
-                                        Conecte-se usando sua conta do Google.
-                                    </DialogDescription>
-                                </DialogHeader>
-
-                                <Button variant="outline" className="gap-1 font-bold" onClick={handleLoginWithGoogleClick}>
-                                    <Image src="/google.svg" width={18} height={18} alt="Google" />
-                                    Google
-                                </Button>
+                                <SignInDialog />
                             </DialogContent>
                         </Dialog>
                     </>
